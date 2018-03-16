1a49e741152a9c7c78efb6a7900aa743f1d88f35
Brandon Valley <bavvh8@mst.edu>
2018-03-15 19:51:28 -0500

    added h file

--- test_case_printer/test_case_printer.cpp
+++ test_case_printer/test_case_printer.cpp
@@ -1,25 +1,8 @@
 
-#include <iostream>
-#include <string>
-#include <typeinfo>
-
-using std::string;
-using namespace std;
+#include "test_case_printer.h"
 
 
 
-struct test_case
-{
-    string lable;             	//need
-    string data;				//need
-    int max_data_chars = 10;
-    string alignment_type = "right";
-    string lable_data_seperation_spacing = " ";
-    string lable_data_seperation_str = ":";
-};
-
-string make_aligned_str(string data, int max_data_chars, string alignment_type);
-void display_test_cases(test_case test_cases[], int num_test_cases, string pre_spaces, string between_str, string post_str, string print_type);
 
 void display_test_cases(test_case test_cases[], int num_test_cases, string pre_spaces = "  ",
 						string between_str = "   " , string post_str = "", string print_type = "one line")
---
+++ test_case_printer/test_case_printer.h
@@ -0,0 +1,29 @@
+#ifndef TEST_CASE_PRINTER_H
+#define TEST_CASE_PRINTER_H
+
+
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+using std::string;
+using namespace std;
+
+
+struct test_case
+{
+    string lable;             	//need
+    string data;				//need
+    int max_data_chars = 10;
+    string alignment_type = "right";
+    string lable_data_seperation_spacing = " ";
+    string lable_data_seperation_str = ":";
+};
+
+string make_aligned_str(string data, int max_data_chars, string alignment_type);
+
+void display_test_cases(test_case test_cases[], int num_test_cases, string pre_spaces, string between_str, string post_str, string print_type);
+
+
+
+#endif
